5f8e1f4e56dfc8a0d536860cc810a40430d81d86
Caihua Du <ducaihua@live.com>
2026-01-04 14:13:42 -0800

    Added a few validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,16 @@
 class Product < ApplicationRecord
   has_one_attached :image
   after_commit -> { broadcast_replace_later_to "products" }
+  validates :title, :description, :price, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :title, uniqueness: true
+  validate :acceptable_image
+
+  def acceptable_image
+    return unless image.attached?
+    acceptable_types = [ "image/jpeg", "image/png", "image/gif" ]
+    unless acceptable_types.include?(image.content_type)
+      errors.add(:image, "must be a JPEG, PNG, or GIF")
+    end
+  end
 end
